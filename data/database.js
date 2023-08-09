62a288ebf7e931376e9b6aaecd36375e544ef4db
Shuklashashi0706 <shuklashashi169@gmail.com>
2023-08-10 02:11:44 +0530

    Node js updated

--- data/database.js
+++ data/database.js
@@ -9,7 +9,7 @@ export const connectDB = () => {
     .connect(process.env.MONGO_URL, {
       dbName: "backendapi",
     })
-    .then(() => console.log("Database Connected"))
+    .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => {
       console.log(e);
     });
